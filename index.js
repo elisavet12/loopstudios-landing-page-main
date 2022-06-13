4d522d453ff9ca59e2db9b2ddd7d124953ae60ff
elisavet12 <85793200+elisavet12@users.noreply.github.com>
2022-06-13 16:12:57 +0300

    ....

--- index.js
+++ index.js
@@ -2,22 +2,25 @@ var burger = document.querySelector("button.burger");
 var close = document.querySelector("button.close");
 var nav = document.querySelector("nav.nav--main");
 var mediaQuery = window.matchMedia("(min-width: 768px)");
+var body = document.querySelector("body");
 
 function openMenu() {
   //   veil.classList.add("veil--open");
   nav.classList.add("nav--open");
+  body.classList.add("noScroll");
 }
 
 function closeMenu() {
   //   veil.classList.remove("veil--open");
   nav.classList.remove("nav--open");
+  body.classList.remove("noScroll");
 }
 
-function isMobile() {
-  if (mediaQuery.matches) {
-    closeMenu();
-  }
-}
+// function isMobile() {
+//   if (mediaQuery.matches) {
+//     closeMenu();
+//   }
+// }
 
 burger.addEventListener("click", openMenu);
 close.addEventListener("click", closeMenu);
