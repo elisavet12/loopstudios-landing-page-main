4ee6bffed7d1944e63ae5c796f7da34c460fff4b
elisavet12 <85793200+elisavet12@users.noreply.github.com>
2022-06-16 18:58:41 +0300

    done!

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 var burger = document.querySelector("button.burger");
 var close = document.querySelector("button.close");
 var nav = document.querySelector("nav.nav--main");
-// var mediaQuery = window.matchMedia("(min-width: 768px)");
 var body = document.querySelector("body");
 
 function openMenu() {
-  //   veil.classList.add("veil--open");
   nav.classList.add("nav--open");
   body.classList.add("noScroll");
 }
